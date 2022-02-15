169ccfd844f8d65f7d9d4c72565fe0e4c5634efe
Yanick Paco Hagemeijer <y.p.hagemeijer@rug.nl>
2022-02-15 12:18:05 +0100

    Just adding the usual obfuscation code

--- add_num.py
+++ add_num.py
@@ -1,12 +1,15 @@
 import sys
 
-def main(a : int, b : int) -> int:
+def add_up_2_ints(a : int, b : int) -> int:
 	summed_ints = a + b
 	print("Ints sum up to a total of:", summed_ints, sep="\t")
 	return summed_ints
 
+def main(arg_list : list):
+	args = [ int(arg) for arg in arg_list ]
+	if len(args) >= 2:
+		result = add_up_2_ints(args[0], args[1])
 
-args = [ int(arg) for arg in sys.argv[1:] ]
-if len(args) >= 2:
-	result = main(args[0], args[1])
 
+if __name__ == "__main__":
+	main(sys.argv[1:])
